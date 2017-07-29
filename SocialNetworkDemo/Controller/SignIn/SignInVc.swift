01f63937a3f10dfb0fc92d10cecad26e15262676
Zabingo Softwares <goutam@zabingo.com>
2017-07-29 19:30:18 +0530

    Completed Setting Up Firebase Integration

---
+++ SocialNetworkDemo/Controller/SignIn/SignInVc.swift
@@ -0,0 +1,17 @@
+//
+//  SignInVc.swift
+//  SocialNetworkDemo
+//
+//  Created by Zabingo Softwares on 29/07/17.
+//  Copyright © 2017 Zabingo Softwares. All rights reserved.
+//
+
+import UIKit
+
+class SignInVc: BaseVc {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+}
